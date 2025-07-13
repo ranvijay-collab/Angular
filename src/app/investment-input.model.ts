f0c96c436a235532f0f99bc9c4c3aa4f04677be7
Ranvijay <thakur.ranvijay12@gmail.com>
2025-07-13 13:39:39 +0530

    calculate

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,16 @@
+export interface InvestmentInput {
+    initialInvestment:number;
+    duration:number;
+    expectedReturn:number;
+    annualInvestment:number;
+}
+
+/* We can declaire like above or Bellow */
+
+// type InvestmentInput = {
+//     initialInvestment:number;
+//     duration:number;
+//     expectedReturn:number;
+//     annualInvestment:number;
+
+// }
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import type { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  @Output() calculater = new EventEmitter<InvestmentInput>();
+
+enterIntialInvestment = '0';
+enterAnnualInvestment= '0';
+enterExpectedReturn='5';
+enterDuration='10';
+  onSubmitInvestment(){
+    this.calculater.emit({
+      initialInvestment: + this.enterIntialInvestment,
+      duration: + this.enterDuration,
+      expectedReturn: + this.enterExpectedReturn,
+      annualInvestment: + this.enterAnnualInvestment
+    }
+    )
+
+  }
+
+}
